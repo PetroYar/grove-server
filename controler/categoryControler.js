3aed728860b416f99315f99697cbbc4ee3331e1b
PetroYar <petryarish@gmail.com>
2025-04-02 23:16:03 +0300

    file clodynary category

--- controler/categoryControler.js
+++ controler/categoryControler.js
@@ -1,71 +1,219 @@
+// import cloudinary from "cloudinary";
+// import fs from "fs";
+// import Category from "../models/Category.js";
+// import mongoose from "mongoose";
+// import slugify from "slugify";
+
+// const categoryControler = {
+//   create: async (req, res) => {
+//     console.log(33);
+//     try {
+//       if (!req.file) {
+//         return res.status(400).json({ error: "Фото обов'язкове" });
+//       }
+//       const result = await cloudinary.v2.uploader.upload(req.file.path);
+//       fs.unlink(req.file.path, (err) => {
+//         if (err) {
+//           console.log("Помилка видалення файлу з локальної папки:", err);
+//         }
+//       });
+
+//       const { name, description } = req.body;
+
+//       if (!name) {
+//         return res.status(400).json({ error: "Імя обов'язкове" });
+//       }
+//       if (!description) {
+//         return res.status(400).json({ error: "Опис обов'язковий" });
+//       }
+//       const slugi = slugify(name, {
+//         replacement: "-",
+//         remove: /[*+~.()'"!:@]/g,
+//         lower: true,
+//       });
+//       const newCategory = new Category({
+//         name,
+//         description,
+//         slug: slugi,
+//         image: result.secure_url,
+//         imageId: result.public_id,
+//       });
+//       await newCategory.save();
+
+//       res.status(201).json(newCategory);
+//     } catch (error) {
+//       res.status(500).json({ error: error.message });
+//     }
+//   },
+//   deleteOne: async (req, res) => {
+//     try {
+//       const { id } = req.params;
+//       console.log(id);
+//       const category = await Category.findById(id);
+
+//       if (!category) {
+//         return res.status(404).json({ error: "Категорія не знайдена" });
+//       }
+
+//       await cloudinary.v2.uploader.destroy(
+//         category.imageId,
+//         (error, result) => {
+//           if (error) {
+//             return res
+//               .status(500)
+//               .json({ error: "Помилка видалення зображення" });
+//           }
+//         }
+//       );
+
+//       await Category.findByIdAndDelete(id);
+
+//       res.status(200).json({ message: "Категорію видалено" });
+//     } catch (error) {
+//       res.status(500).json({ error: "Помилка сервера" });
+//     }
+//   },
+//   getAll: async (req, res) => {
+//     try {
+//       const categories = await Category.find();
+//       if (categories.length === 0) {
+//         return res.status(404).json({ message: "Категорії не знайдені" });
+//       }
+//       res.status(200).json(categories);
+//     } catch (error) {
+//       console.error("Помилка при отриманні категорій:", error);
+//       res.status(500).json({ error: "Помилка сервера" });
+//     }
+//   },
+//   update: async (req, res) => {
+//     try {
+//       const { id } = req.params;
+//       const { name, description } = req.body;
+
+//       if (!mongoose.Types.ObjectId.isValid(id)) {
+//         return res.status(400).json({ error: "Невірний ID категорії" });
+//       }
+
+//       const category = await Category.findById(id);
+//       if (!category) {
+//         return res.status(404).json({ error: "Категорію не знайдено" });
+//       }
+
+//       let updatedImage = category.image;
+//       let updatedImageId = category.imageId;
+
+//       if (req.file) {
+//         console.log("File received:", req.file);
+
+//         const result = await cloudinary.v2.uploader.upload(req.file.path);
+//         updatedImage = result.secure_url;
+//         updatedImageId = result.public_id;
+
+//         if (category.imageId) {
+//           console.log("Deleting old image from Cloudinary...");
+//           await cloudinary.v2.uploader.destroy(category.imageId);
+//         }
+
+//         fs.unlink(req.file.path, (err) => {
+//           if (err) {
+//             console.log("Помилка видалення файлу з локальної папки:", err);
+//           }
+//         });
+//       } else {
+//         console.log("No file received");
+//       }
+//       const slugi = slugify(name, {
+//         replacement: "-",
+//         remove: /[*+~.()'"!:@]/g,
+//         lower: true,
+//       });
+//       const updatedCategory = await Category.findByIdAndUpdate(
+//         id,
+//         {
+//           name,
+//           description,
+//           slug: slugi,
+//           image: updatedImage,
+//           imageId: updatedImageId,
+//         },
+//         { new: true }
+//       );
+
+//       res.status(200).json(updatedCategory);
+//     } catch (error) {
+//       console.error("Помилка при оновленні категорії:", error);
+//       res.status(500).json({ error: "Помилка сервера" });
+//     }
+//   },
+// };
+
+// export default categoryControler;
 import cloudinary from "cloudinary";
-import fs from "fs";
-import Category from "../models/Category.js";
 import mongoose from "mongoose";
 import slugify from "slugify";
+import Category from "../models/Category.js";
 
-const categoryControler = {
+const categoryController = {
   create: async (req, res) => {
-    console.log(33)
     try {
       if (!req.file) {
         return res.status(400).json({ error: "Фото обов'язкове" });
       }
-      const result = await cloudinary.v2.uploader.upload(req.file.path);
-      fs.unlink(req.file.path, (err) => {
-        if (err) {
-          console.log("Помилка видалення файлу з локальної папки:", err);
-        }
-      });
+
+      const fileBuffer = req.file.buffer;
+      const base64Data = fileBuffer.toString("base64");
+      const fileUri = `data:${req.file.mimetype};base64,${base64Data}`;
+
+      const result = await cloudinary.v2.uploader.upload(fileUri);
 
       const { name, description } = req.body;
 
       if (!name) {
-        return res.status(400).json({ error: "Імя обов'язкове" });
+        return res.status(400).json({ error: "Ім'я обов'язкове" });
       }
       if (!description) {
         return res.status(400).json({ error: "Опис обов'язковий" });
       }
-      const slugi = slugify(name, {
+
+      const slug = slugify(name, {
         replacement: "-",
         remove: /[*+~.()'"!:@]/g,
         lower: true,
       });
+
       const newCategory = new Category({
         name,
         description,
-        slug: slugi,
+        slug,
         image: result.secure_url,
         imageId: result.public_id,
       });
+
       await newCategory.save();
 
       res.status(201).json(newCategory);
     } catch (error) {
-     
       res.status(500).json({ error: error.message });
     }
   },
+
   deleteOne: async (req, res) => {
     try {
       const { id } = req.params;
-      console.log(id);
+
       const category = await Category.findById(id);
 
       if (!category) {
         return res.status(404).json({ error: "Категорія не знайдена" });
       }
 
-      await cloudinary.v2.uploader.destroy(
-        category.imageId,
-        (error, result) => {
-          if (error) {
-            return res
-              .status(500)
-              .json({ error: "Помилка видалення зображення" });
-          }
+      await cloudinary.v2.uploader.destroy(category.imageId, (error) => {
+        if (error) {
+          return res
+            .status(500)
+            .json({ error: "Помилка видалення зображення" });
         }
-      );
+      });
 
       await Category.findByIdAndDelete(id);
 
@@ -74,6 +222,7 @@ const categoryControler = {
       res.status(500).json({ error: "Помилка сервера" });
     }
   },
+
   getAll: async (req, res) => {
     try {
       const categories = await Category.find();
@@ -82,10 +231,10 @@ const categoryControler = {
       }
       res.status(200).json(categories);
     } catch (error) {
-      console.error("Помилка при отриманні категорій:", error);
       res.status(500).json({ error: "Помилка сервера" });
     }
   },
+
   update: async (req, res) => {
     try {
       const { id } = req.params;
@@ -104,36 +253,31 @@ const categoryControler = {
       let updatedImageId = category.imageId;
 
       if (req.file) {
-        console.log("File received:", req.file);
+        const fileBuffer = req.file.buffer;
+        const base64Data = fileBuffer.toString("base64");
+        const fileUri = `data:${req.file.mimetype};base64,${base64Data}`;
 
-        const result = await cloudinary.v2.uploader.upload(req.file.path);
+        const result = await cloudinary.v2.uploader.upload(fileUri);
         updatedImage = result.secure_url;
         updatedImageId = result.public_id;
 
         if (category.imageId) {
-          console.log("Deleting old image from Cloudinary...");
           await cloudinary.v2.uploader.destroy(category.imageId);
         }
-
-        fs.unlink(req.file.path, (err) => {
-          if (err) {
-            console.log("Помилка видалення файлу з локальної папки:", err);
-          }
-        });
-      } else {
-        console.log("No file received");
       }
-      const slugi = slugify(name, {
+
+      const slug = slugify(name, {
         replacement: "-",
         remove: /[*+~.()'"!:@]/g,
         lower: true,
       });
+
       const updatedCategory = await Category.findByIdAndUpdate(
         id,
         {
           name,
           description,
-          slug: slugi,
+          slug,
           image: updatedImage,
           imageId: updatedImageId,
         },
@@ -142,10 +286,9 @@ const categoryControler = {
 
       res.status(200).json(updatedCategory);
     } catch (error) {
-      console.error("Помилка при оновленні категорії:", error);
       res.status(500).json({ error: "Помилка сервера" });
     }
   },
 };
 
-export default categoryControler;
+export default categoryController;
