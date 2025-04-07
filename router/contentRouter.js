ea142dfe2e0c65a466e12c0442c2736ec7b071e5
PetroYar <petryarish@gmail.com>
2025-04-07 12:27:42 +0300

    edit upload

--- router/contentRouter.js
+++ router/contentRouter.js
@@ -1,14 +1,14 @@
 import { Router } from "express";
 import contentControler from "../controler/contentControler.js";
-import uploadMiddleware from "../middleware/uploadMiddleware.js";
+import uploadMiddlewareContent from "../middleware/uploadMiddlewareContent.js";
 
 const contentRouter = Router();
 
-contentRouter.post("/",uploadMiddleware, contentControler.create);
+contentRouter.post("/",uploadMiddlewareContent, contentControler.create);
 contentRouter.get("/page/:page", contentControler.getPageContent);
 contentRouter.get("/pages", contentControler.getPages);
 contentRouter.get("/:key", contentControler.getOne);
-contentRouter.put("/:key",uploadMiddleware, contentControler.update);
+contentRouter.put("/:key",uploadMiddlewareContent, contentControler.update);
 contentRouter.delete("/:key", contentControler.delete);
 
 export default contentRouter;
