15c041957e8e61485ca477e3dfd51085af0d3e6c
PetroYar <petryarish@gmail.com>
2025-04-07 12:43:47 +0300

    test buffer upload

--- router/contentRouter.js
+++ router/contentRouter.js
@@ -1,14 +1,14 @@
 import { Router } from "express";
 import contentControler from "../controler/contentControler.js";
-import uploadMiddlewareContent from "../middleware/uploadMiddlewareContent.js";
+import uploadMiddleware from "../middleware/uploadMiddleware.js";
 
 const contentRouter = Router();
 
-contentRouter.post("/",uploadMiddlewareContent, contentControler.create);
+contentRouter.post("/",uploadMiddleware, contentControler.create);
 contentRouter.get("/page/:page", contentControler.getPageContent);
 contentRouter.get("/pages", contentControler.getPages);
 contentRouter.get("/:key", contentControler.getOne);
-contentRouter.put("/:key",uploadMiddlewareContent, contentControler.update);
+contentRouter.put("/:key",uploadMiddleware, contentControler.update);
 contentRouter.delete("/:key", contentControler.delete);
 
 export default contentRouter;
